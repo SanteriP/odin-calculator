5a180f25b787eaf82b99bf18d73aa1c4fef4dec1
SanteriP <santeri.passi@gmail.com>
2024-04-12 23:10:50 +0300

    Add backspace functionality

--- script.js
+++ script.js
@@ -3,23 +3,50 @@ let operand2 = "";
 operator = "";
 total = "";
 
+let screenNumbers = document.querySelector(".screen-numbers");
+
 addEventListener('keydown', (event) => {
+    if(total !== "") {
+        total = "";
+        console.log("Total reset");
+        screenNumbers.textContent = "";
+    }
+
+    if(event.key === "Backspace" && operand2 === "" && operator === "") {
+        operand1 = operand1.slice(0,-1);
+        screenNumbers.textContent = screenNumbers.textContent.slice(0,-1);
+    }
+
+    if(event.key === "Backspace" && operand2 !== "") {
+        operand2 = operand2.slice(0,-1);
+        if (operand1.length > 0) {
+            screenNumbers.textContent = screenNumbers.textContent.slice(0,-1);
+        }
+    }
+
+
+
     if((event.key >= 0 || event.key <= 9) && operator === "") {
         operand1 += event.key;
         console.log(`operand1: ${operand1}`);
+        screenNumbers.textContent += event.key
     }
     else if ((event.key >= 0 || event.key <= 9) && operator != "") {
         operand2 += event.key;
         console.log(`operand2: ${operand2}`)
+        screenNumbers.textContent += event.key;
     };
 
     if((event.key === "+"
     || event.key === "-"
     || event.key === "*"
     || event.key === "/")
+    && operand1 !== ""
     && operand2 === "") {
+        event.preventDefault();
         operator = event.key;
         console.log(operator);
+        screenNumbers.textContent += event.key
     };
 
     if((event.key === "+"
@@ -27,22 +54,24 @@ addEventListener('keydown', (event) => {
     || event.key === "*"
     || event.key === "/")
     && operand2 !== "") {
+        event.preventDefault();
         operand1 = operate(operator,+operand1,+operand2);
         operator = event.key;
         operand2 = "";
         console.log(operator);
         console.log(`operand1 after operation: ${operand1}`)
+        screenNumbers.textContent = operand1 + event.key;
     };
 
     if(event.key === "Enter"
     && (operand1 !== "")
     && (operand2 !== "")) {
-        result = operate(operator,+operand1,+operand2);
-        console.log(result);
+        total = operate(operator,+operand1,+operand2);
+        console.log(total);
         operand1 = "";
         operand2 = "";
         operator = "";
-
+        screenNumbers.textContent = total;
     };
 });
 
@@ -57,6 +86,6 @@ function operate(operator,operand1,operand2) {
         return operand1 * operand2;
     };
     if (operator === "/") {
-        return operand1 / operand2;
+        return (operand1 / operand2).toFixed(3);
     };
 }
